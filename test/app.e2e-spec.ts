66a0ea5bdc409604669b2709b6d44676f949c26d
Ashim-Stha <ashimshrestha2384@gmail.com>
2025-01-24 16:38:41 +0545

    signup e2e test

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,6 +1,9 @@
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { Test } from '@nestjs/testing';
+import * as pactum from 'pactum';
+import { emitWarning } from 'process';
 import { AppModule } from 'src/app.module';
+import { AuthDto } from 'src/auth/dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 describe('App e2e', () => {
@@ -17,9 +20,11 @@ describe('App e2e', () => {
     app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
     await app.init();
+    await app.listen(3333);
 
     prisma = app.get(PrismaService);
     await prisma.cleanDb();
+    pactum.request.setBaseUrl('http:localhost:3333');
   });
 
   //teardown logic
@@ -30,7 +35,21 @@ describe('App e2e', () => {
   it.todo('should pass');
 
   describe('Auth', () => {
-    describe('Signup', () => {});
+    describe('Signup', () => {
+      it('should signup ', () => {
+        const dto: AuthDto = {
+          email: 'luffy@gmail.com',
+          password: 'luffy',
+        };
+
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody(dto)
+          .expectStatus(201);
+        // .inspect();
+      });
+    });
 
     describe('Signin', () => {});
   });
